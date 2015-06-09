e48e8a2ba681258b5ce669a09e4219f43422df0d
dauletmoldabayev <daulet.msu@gmail.com>
2015-06-09 19:03:52 +0200

    Final improvements to pollutantmean.R .

--- pollutantmean.R
+++ pollutantmean.R
@@ -1,7 +1,10 @@
 # Programming Assignment 1: Air Pollution
 # Prepared by Daulet Moldabayev 
 
-pollutantmean <- function(directory = "specdata", pollutant, id = 1){
+pollutantmean <- function(directory = "specdata", pollutant, id = 1:10){
+        # set the working directory to the 'directory'
+        setwd(directory)
+        
         # testing that 'pollutant' got a correct value: "sulfate" or "nitrate"
         if(!identical(pollutant,"sulfate") & !identical(pollutant,"nitrate")){
                 print("Error: pollutant name is not correct!")
@@ -12,8 +15,8 @@ pollutantmean <- function(directory = "specdata", pollutant, id = 1){
         # directory
         
         # 1st: compute number of files in the 'directory'
-        number_of_files <- length(list.files(directory, 
-                                             pattern = "*.csv", full.names = T))
+        number_of_files <- length(list.files( 
+                                            pattern = "*.csv", full.names = T))
         # 2nd: check if values of 'id' are 
         # in range between 1 and 'number_of_files'
         if( length(id[id >0 & id < number_of_files + 1]) != length(id) ){
@@ -26,16 +29,14 @@ pollutantmean <- function(directory = "specdata", pollutant, id = 1){
                 names <- vector( length(id), mode = "character" )
                 for(i in 1:length(id)){
                         if(id[i]<10){
-                                names[i] <- paste( directory, "/", 0, 0, id[i], 
-                                           ".csv", sep = "", collapse = "")
+                                names[i] <- paste( 0, 0, id[i], 
+                                                        ".csv", sep = "")
                         }
                         else if(id[i]>9 & id[i]<100){
-                                names[i] <- paste( directory, "/", 0, id[i], 
-                                           ".csv", sep = "", collapse = "")        
+                                names[i] <- paste( 0, id[i], ".csv", sep = "")        
                         } 
                         else{
-                                names[i] <- paste( directory, "/", id[i], 
-                                        ".csv", sep = "", collapse = "")
+                                names[i] <- paste( id[i], ".csv", sep = "")
                         }
                 }
                 names
@@ -69,5 +70,7 @@ pollutantmean <- function(directory = "specdata", pollutant, id = 1){
         print(paste(c("Mean value for ", pollutant, " accross file ids ", 
                 as.character(min(id)), ":" , as.character(max(id)),  
                         " is computed!!!"), collapse = ""))
+        # set the working directory to back to the main directory
+        setwd("..")
         cleandata[1]/cleandata[2]
 }
